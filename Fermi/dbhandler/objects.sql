64fffd6331edceefc1f0d6329874f6ae87a323a1
Dmitriy Blinov <dmitriy.blinov@gmail.com>
2013-01-19 16:48:54 +0400

    added q1611 to objects.sql

--- Fermi/dbhandler/objects.sql
+++ Fermi/dbhandler/objects.sql
@@ -1,5 +1,6 @@
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("S4 0954");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("BL Lacertae");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("Q1633");
+INSERT INTO lacertabase.Fermi_objects (name) VALUES ("Q1611");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("PKS1510");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("3C 66A");
