c3cfaa20104c5875031d1cc5d7e1949683e57b5f
Dmitriy Blinov <dmitriy.blinov@gmail.com>
2013-02-08 22:34:20 +0400

    Mkn421 added to DB

--- Fermi/dbhandler/objects.sql
+++ Fermi/dbhandler/objects.sql
@@ -4,3 +4,4 @@ INSERT INTO lacertabase.Fermi_objects (name) VALUES ("Q1633");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("Q1611");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("PKS1510");
 INSERT INTO lacertabase.Fermi_objects (name) VALUES ("3C 66A");
+INSERT INTO lacertabase.Fermi_objects (name) VALUES ("Mkn421");
